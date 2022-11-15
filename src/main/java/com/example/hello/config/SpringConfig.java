fd2f4bf93875f68723c7538924a7eb754a96c5d9
haewon <n2203020@N2203020ui-MacBookPro.local>
2022-11-15 19:00:58 +0900

    Add signup

---
+++ src/main/java/com/example/hello/config/SpringConfig.java
@@ -0,0 +1,19 @@
+//package com.example.hello.config;
+//
+//import com.example.hello.repository.MemberRepository;
+//import com.example.hello.repository.MemoryMemberRepository;
+//import com.example.hello.service.MemberService;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class SpringConfig {
+//    @Bean
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository());
+//    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+//}

04cf2012d9f88c946cbd1aaadc5927823858a7ef
haewon <n2203020@N2203020ui-MacBookPro.local>
2022-11-15 19:04:49 +0900

    Edit signup

--- src/main/java/com/example/hello/service/MemberService.java
+++ src/main/java/com/example/hello/service/MemberService.java
@@ -21,6 +21,7 @@ public class MemberService {
      */
     public Long join(Member member) {
         validateDuplicateMember(member); //중복 회원 검증 memberRepository.save(member);
+        memberRepository.save(member);
         return member.getId();
     }
     private void validateDuplicateMember(Member member) {
